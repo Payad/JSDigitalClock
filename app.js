fb91a78a5aecfc20590ba1241ff18bb9f85da198
Payad <swordoflight8847@yahoo.com>
2023-04-20 15:17:43 -0700

    set event listener for clockBtn

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-setInterval(setDigitalClock, 1000)
+const clrInt = setInterval(setDigitalClock, 1000)
 
 function setDigitalClock() {
     const currentDate = new Date();
     const hourRatio = currentDate.getHours() > 12 ? currentDate.getHours() - 12 : currentDate.getHours();
-    const minuteRatio = currentDate.getMinutes();
+    const minuteRatio = ('0' + currentDate.getMinutes()).slice(-2);
     const secondRatio = currentDate.getSeconds();
 
     setTime(hourEl, hourRatio);
@@ -12,7 +12,17 @@ function setDigitalClock() {
 
 const hourEl = document.querySelector('.hour');
 const minEl = document.querySelector('.minute');
+const separator = document.querySelector('.separator');
 
 function setTime(element, timeRatio) {
     element.innerHTML = parseInt(timeRatio);
 }
+
+const clockBtn = document.querySelector('.clockBtn');
+
+clockBtn.addEventListener('click', () => {
+    clearInterval(clrInt);
+    hourEl.style.setProperty('animation', 'separator 1s steps(1, end) infinite');
+    minEl.style.setProperty('animation', 'separator 1s steps(1, end) infinite');
+    separator.style.setProperty('animation', 'none');
+})
